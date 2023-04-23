8ff19213274e5572ac9d3aa52a484ab2e350309b
SteVioli <stefano.violi87@gmail.com>
2023-04-23 18:34:07 +0200

    prova cors

--- src/main/java/com/EasyLoadGestioneImpresa/auth/controller/AuthController.java
+++ src/main/java/com/EasyLoadGestioneImpresa/auth/controller/AuthController.java
@@ -15,6 +15,7 @@ import com.EasyLoadGestioneImpresa.auth.payload.RegisterDto;
 import com.EasyLoadGestioneImpresa.auth.service.AuthService;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/auth")
 public class AuthController {
 
